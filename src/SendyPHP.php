d2a32a07b800246122008749ed6aaad8f5d3387f
Manuel Voss <mv@moay.de>
2014-04-06 11:27:19 +0200

    initial commit, added redirect fix

--- src/SendyPHP.php
+++ src/SendyPHP.php
@@ -206,14 +206,64 @@ class SendyPHP
         $postdata = http_build_query($content);
 
         $ch = curl_init($this->installation_url .'/'. $type);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
-        $result = curl_exec($ch);
+        $redirects = 0;
+        // Remove the need to have CURL_FOLLOWREDIRECTS
+        $result = $this->curl_redirect_exec($ch, $redirects, true, 10, true, $postdata);
         curl_close($ch);
 
         return $result;
     }
+
+    /**
+     * @param cURL  $ch                     
+     * @param int   $redirects              
+     * @param bool  $curlopt_returntransfer 
+     * @param int   $curlopt_maxredirs      
+     * @param bool  $curlopt_header         
+     * @param string $postdata
+     * @return mixed
+     */
+    private function curl_redirect_exec($ch, &$redirects, $curlopt_returntransfer = false, $curlopt_maxredirs = 10, $curlopt_header = false, $postdata = '') {
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+        $data = curl_exec($ch);
+        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $exceeded_max_redirects = $curlopt_maxredirs > $redirects;
+        $exist_more_redirects = false;
+        if ($http_code == 301 || $http_code == 302) {
+            if ($exceeded_max_redirects) {
+                list($header) = explode("\r\n\r\n", $data, 2);
+                $matches = array();
+                preg_match('/(Location:|URI:)(.*?)\n/', $header, $matches);
+                $url = trim(array_pop($matches));
+                $url_parsed = parse_url($url);
+                if (isset($url_parsed)) {
+                    curl_setopt($ch, CURLOPT_URL, $url);
+                    $redirects++;
+                    return curl_redirect_exec($ch, $redirects, $curlopt_returntransfer, $curlopt_maxredirs, $curlopt_header);
+                }
+            }
+            else {
+                $exist_more_redirects = true;
+            }
+        }
+        if ($data !== false) {
+            if (!$curlopt_header)
+                list(,$data) = explode("\r\n\r\n", $data, 2);
+            if ($exist_more_redirects) return false;
+            if ($curlopt_returntransfer) {
+                return $data;
+            }
+            else {
+                echo $data;
+                if (curl_errno($ch) === 0) return true;
+                else return false;
+            }
+        }
+        else {
+            return false;
+        }
+    }
 }
+
